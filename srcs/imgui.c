4ff2c34d1a7daec8d41cf6e5793558c40d04fddb
Emmanuel Florenzano <emmanuelflo@hotmail.fr>
2018-01-20 01:07:26 +0100

    slider thumb

--- srcs/imgui.c
+++ srcs/imgui.c
@@ -79,7 +79,7 @@ int	button(t_window *win, t_ui_state *ui, int id, t_rect_int rect, char *str)
 	return (0);
 }
 
-int	slider(t_window *win, t_ui_state *ui, int id, t_rect_int rect, int max, int *value)
+/*int	slider(t_window *win, t_ui_state *ui, int id, t_rect_int rect, int max, int *value)
 {
 	int		v;
 	int		mousepos;
@@ -143,11 +143,95 @@ int	slider(t_window *win, t_ui_state *ui, int id, t_rect_int rect, int max, int 
 		}
 	}
 	return (0);
+}*/
+
+int	slider(t_window *win, t_ui_state *ui, int id, t_rect_int rect, int max, int *value)
+{
+	int	ret;
+
+	g_rect_fill(win->img_next, rect, 0x777777);
+	ret = slider_thumb(win, ui, id, rect, max, value);
+	if (regionhit(ui, rect))
+	{
+		ui->hotitem = id;
+		if (ui->activeitem == 0 && ui->mousedown)
+			ui->activeitem = id;
+	}
+	return (ret);
+}
+
+
+int	slider_thumb(t_window *win, t_ui_state *ui, int id, t_rect_int rect, int max, int *value)
+{
+	int		v;
+	int		mousepos;
+	t_rect_int	thumb;
+
+
+	//g_rect_fill(win->img_next, rect, 0x777777);
+	if (rect.w < rect.h)
+	{
+		thumb.x = rect.x + 4;
+		thumb.h = rect.h / 16;
+		thumb.y = rect.y + 4 + (*value * (rect.h - 8 - thumb.h)) / max;
+		thumb.w = rect.w - 8;
+	}
+	else
+	{
+		thumb.y = rect.y + 4;
+		thumb.w = rect.w / 16;
+		thumb.x = rect.x + 4 + (*value * (rect.w - 8 - thumb.w)) / max;
+		thumb.h = rect.h - 8;
+
+	}
+	if (regionhit(ui, thumb))
+	{
+		ui->hotitem = id;
+		if (ui->activeitem == 0 && ui->mousedown)
+			ui->activeitem = id;
+	}
+	else if (ui->hotitem == id)
+		ui->hotitem = 0;
+	if (ui->activeitem == id && !ui->mousedown)
+		ui->activeitem = 0;
+	if (ui->activeitem == id || ui->hotitem == id)
+		g_rect_fill(win->img_next, thumb, 0xFFFFFF);
+	else
+		g_rect_fill(win->img_next, thumb, 0xAAAAAA);
+	if (ui->activeitem == id)
+	{
+		if (rect.w < rect.h)
+		{
+			mousepos = ui->mousey - (rect.y + 4);
+			if (mousepos < 0)
+				mousepos = 0;
+			if (mousepos > rect.h - 8)
+				mousepos = rect.h - 8;
+			v = mousepos * max / (rect.h - 8);
+		}
+		else
+		{
+			mousepos = ui->mousex - (rect.x + 4);
+			if (mousepos < 0)
+				mousepos = 0;
+			if (mousepos > rect.w - 8)
+				mousepos = rect.w - 8;
+			v = mousepos * max / (rect.w - 8);
+		}
+		if (v != *value)
+		{
+			printf("V %d\n", v);
+			*value = v;
+			return (1);
+		}
+	}
+	return (0);
 }
 
 int	color_picker(t_window *win, t_ui_state *ui, int id, t_rect_int rect, t_hsv *color, t_hsv *color_select)
 {
-	int	hue_value;
+	int		hue_value;
+	t_rect_int	slider_rect;
 
 	if (regionhit(ui, make_rect(rect.x, rect.y, rect.w, rect.h - 32)))
 	{
@@ -161,7 +245,9 @@ int	color_picker(t_window *win, t_ui_state *ui, int id, t_rect_int rect, t_hsv *
 		ui->activeitem = 0;
 	g_rect_fill(win->img_next, rect, 0x777777);
 	hue_value = (int)(color->hue * 1000 / 360);
-	slider(win, ui, 255, make_rect(rect.x, rect.y + rect.h - 32, rect.w, 32), 1000, &hue_value);
+	slider_rect = make_rect(rect.x, rect.y + rect.h - 32, rect.w, 32);
+	g_rect_hue_gradient(win->img_next, make_rect(slider_rect.x + 4, slider_rect.y + 4, slider_rect.w - 8, slider_rect.h - 8));
+	slider_thumb(win, ui, id + 1, slider_rect, 1000, &hue_value);
 	color->hue = hue_value / 1000.0 * 360.0;
 	g_rect_hsv_gradient(win->img_next, make_rect(rect.x, rect.y, rect.w, rect.h - 32), color->hue);
 	if (ui->hotitem == id)
